6d7c64d18232bf08a9565a4a31139cad56cbab0a
Swiiezzy <105414607+Swiiezzy@users.noreply.github.com>
2022-08-20 13:39:28 +0200

    Update question.js

--- quizz/src/Component/question.js
+++ quizz/src/Component/question.js
@@ -5,12 +5,12 @@ export default function Question(props) {
     const { click, id, end } = props
     const { answers, category, difficulty, question } = props.question
 
- 
+
 
     const rQuestion = answers.map(item => {
         if (end) {
-if (item.correct){ var color = "green"}
-else if (item.correct === false && item.click === true ){ var color ="red"}
+            if (item.correct) { var color = "green" }
+            else if (item.correct === false && item.click === true) { var color = "red" }
             return (<p
                 key={item.answer}
                 className="question"
@@ -34,16 +34,16 @@ else if (item.correct === false && item.click === true ){ var color ="red"}
 
 
 
-return (
-    <div className="question--folder--main button-85">
-        <h2>{question}</h2>
-        <div className="question--folder ">
-            {rQuestion}
+    return (
+        <div className="question--folder--main button-85">
+            <h2>{question}</h2>
+            <div className="question--folder ">
+                {rQuestion}
+            </div>
+            <div className="info">
+                <p>Category :{category}</p>
+                <p>Difficulty :{difficulty}</p>
+            </div>
         </div>
-        <div className="info">
-            <p>Category :{category}</p>
-            <p>Difficulty :{difficulty}</p>
-        </div>
-    </div>
-)
+    )
 }
